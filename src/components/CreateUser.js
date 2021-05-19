6f1bdbef9c8b0406a6f2a3da012715c952d690ae
rajeshd <rajesh.de@onometra.com>
2021-05-19 08:13:52 +0530

    CreateUser is refactored into UserForm

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,81 +1,33 @@
 import { useState } from "react";
 
-import Modal from "./Modal";
 import { showToastMessage, firebase } from "../helper";
+import UserForm from "./UserForm";
 
 const CreateUser = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+
   const onCreate = () => setIsOpen(true);
-
-  const onClickCancel = () => {
-    setName("");
-    setEmail("");
-    setIsOpen(false);
-  };
-
-  const onClickSave = () => {
-    if (name && email) {
-      firebase.create({ name, email }, (err) => {
-        if (!err) {
-          onClickCancel();
-          return;
-        }
-        showToastMessage(err.message || "");
-      });
-    } else {
-      showToastMessage("Name and Email is mandatory");
-    }
+  const handleOnClose = () => setIsOpen(false);
+  const handleOnSave = (obj) => {
+    firebase.create(obj, (err) => {
+      if (!err) {
+        handleOnClose();
+        return;
+      }
+      showToastMessage(err.message || "");
+    });
   };
 
   return (
     <>
-      <button type="button" onClick={onCreate}>
-        Create
+      <button
+        onClick={onCreate}
+        className="btn btn-primary margin-top-30"
+        type="button"
+      >
+        Add user
       </button>
-      <Modal isOpen={isOpen} onRequestClose={onClickCancel}>
-        <form>
-          <div className="form-group margin-bottom-30">
-            <label htmlFor="name" className="margin-right-10">
-              Name
-            </label>
-            <input
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-            />
-          </div>
-          <div className="form-group margin-bottom-30">
-            <label htmlFor="email" className="margin-right-10">
-              Email
-            </label>
-            <input
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              type="text"
-            />
-          </div>
-          <div class="d-flex flex-row-reverse">
-            <button
-              onClick={onClickSave}
-              className="p-2 btn btn-primary"
-              type="button"
-            >
-              Save
-            </button>
-            <button
-              onClick={onClickCancel}
-              className="p-2 btn btn-secondary margin-right-10"
-              type="button"
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </Modal>
+      <UserForm isOpen={isOpen} onSave={handleOnSave} onClose={handleOnClose} />
     </>
   );
 };
---
+++ src/components/UserForm.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import { showToastMessage } from "../helper";
+
+const validate = ({ name, email }) => {
+  const isValidEmailAddress = (text) =>
+    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(text);
+
+  if (!name || !email) {
+    showToastMessage("Name and Email is mandatory");
+    return false;
+  }
+  if (!isValidEmailAddress(email)) {
+    showToastMessage("Wrong Email format");
+    return false;
+  }
+
+  return true;
+};
+
+const UserForm = (props) => {
+  const [name, setName] = useState(props.name || "");
+  const [email, setEmail] = useState(props.email || "");
+
+  const onClickCancel = () => {
+    setName("");
+    setEmail("");
+    props.onClose();
+  };
+  const onClickSave = () => {
+    if (validate({ name, email })) {
+      props.onSave({ name, email });
+    }
+  };
+
+  return (
+    <Modal isOpen={props.isOpen} onRequestClose={onClickCancel}>
+      <form>
+        <div className="form-group margin-bottom-30">
+          <label htmlFor="name" className="margin-right-10">
+            Name
+          </label>
+          <input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+          />
+        </div>
+        <div className="form-group margin-bottom-30">
+          <label htmlFor="email" className="margin-right-10">
+            Email
+          </label>
+          <input
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+          />
+        </div>
+        <div className="d-flex flex-row-reverse">
+          <button
+            onClick={onClickSave}
+            className="p-2 btn btn-primary"
+            type="button"
+          >
+            Save
+          </button>
+          <button
+            onClick={onClickCancel}
+            className="p-2 btn btn-secondary margin-right-10"
+            type="button"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default UserForm;
